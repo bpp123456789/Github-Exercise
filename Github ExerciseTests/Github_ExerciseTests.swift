e5ca167db17b9e0e4ab2279acb5c642fd95b173c
william.petrik <billypetrik@icloud.com>
2024-09-01 08:09:06 -0400

    Initial Commit

---
+++ Github ExerciseTests/Github_ExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseTests.swift
+//  Github ExerciseTests
+//
+//  Created by William Petrik on 9/1/24.
+//
+
+import Testing
+@testable import Github_Exercise
+
+struct Github_ExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
